9699518ad89f0b0184325c9cd1c2c835fc7d2469
Yuri Karabatov <yuri@yurikarabatov.com>
2018-12-17 19:26:56 +0300

    Create new project

---
+++ brunchlocator/brunchlocator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  brunchlocator
+//
+//  Created by Yuri Karabatov on 17/12/2018.
+//  Copyright © 2018 Yuri Karabatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
